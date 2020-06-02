bd00e486e481638a8d6a2a5ab267871cb5b0b6a4
Randeep Singh <rand-singh@users.noreply.github.com>
2020-06-02 19:05:55 +0100

    List rendering
    It is recommended to use a special key attribute when rendering elements like lists so Vue can keep track each node's identity

--- main.js
+++ main.js
@@ -6,5 +6,30 @@ var app = new Vue({
     image: "./vmSocks-green-onWhite.jpg",
     inventory: 100,
     onSale: true,
+    details: ["80% Cotton", "20% polyester", "Gender neutral"],
+    variants: [
+      {
+        variandId: 2234,
+        variantColor: "green",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+      },
+    ],
+    sizes: [
+      {
+        sizeId: 1,
+        name: "small",
+      },
+      {
+        sizeId: 2,
+        name: "medium",
+      },
+      {
+        sizeId: 3,
+        name: "large",
+      },
+    ],
   },
 });
