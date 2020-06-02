249f690a64382e4d6f358512db39dd34b74a8a87
Randeep Singh <rand-singh@users.noreply.github.com>
2020-06-02 18:56:59 +0100

    Conditional Rendering
    v-show is more performant than v-if as it simply changes the elements visibility where as v-if adds and removes elements to the DOM. Choose wisely

--- main.js
+++ main.js
@@ -4,5 +4,7 @@ var app = new Vue({
     product: "Socks",
     description: "A pair of warm, fuzzy socks",
     image: "./vmSocks-green-onWhite.jpg",
+    inventory: 100,
+    onSale: true,
   },
 });
